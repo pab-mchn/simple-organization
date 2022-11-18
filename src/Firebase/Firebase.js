8c79bf5f5bd0b7e22e019fcae9b36a33221d1291
pab-mich <pab.michelini@gmail.com>
2022-11-18 10:43:17 +0000

    firebase integrated

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6VZ8381EcGEtE-lkboMILL-VcSZwLwgk",
+  authDomain: "simple-organization-2c6c2.firebaseapp.com",
+  projectId: "simple-organization-2c6c2",
+  storageBucket: "simple-organization-2c6c2.appspot.com",
+  messagingSenderId: "680439548524",
+  appId: "1:680439548524:web:ecf25cb0c2699a5d676649",
+  measurementId: "G-5ZKSBKRH3E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
