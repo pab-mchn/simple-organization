3c2ecf95c41568bf8c3d1f592a62578f2d6d76b5
pab-mich <pab.michelini@gmail.com>
2022-11-21 12:16:29 +0000

    get in real time data with querysnapshot

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,34 +1,34 @@
 import { useEffect, useState } from "react";
 import ActivitiesForm from "../ActivitiesForm/ActivitiesForm";
 
-import { collection, DocumentSnapshot, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../../Firebase/Firebase";
 
 const Activities = () => {
   const [activitie, setActivitie] = useState([]);
 
-  const getActivities = async () => {
-    const activitiesDocs = [];
-    const querySnapshot = await getDocs(collection(db, "activities"));
-    querySnapshot.forEach((doc) => {
-      activitiesDocs.push({ ...doc.data(), id: doc.id });
-      console.log(activitiesDocs);
-      setActivitie(activitiesDocs);
-      console.log(activitie);
-    });
-  };
-
   useEffect(() => {
-    getActivities();
+    onSnapshot(collection(db, "activities"), (querySnapshot) => {
+      const activitiesDocs = [];
+      querySnapshot.forEach((doc) => {
+        activitiesDocs.push({ ...doc.data(), id: doc.id });
+        console.log(activitiesDocs);
+        setActivitie(activitiesDocs);
+        console.log(activitie);
+      });
+    });
   }, []);
   return (
     <>
       <ActivitiesForm />
       <h1>Activities</h1>
       <div>
-        {activitie.map((act) => {
-          return <h1>{act.name}</h1>;
-        })}
+        {activitie.map((act) => (
+          <div key={act.id}>
+            <h2>{act.name}</h2>
+            <h4>{act.description}</h4>
+          </div>
+        ))}
       </div>
     </>
   );
